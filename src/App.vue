8b6e8923fd3dcbc2a3d1893332d387d41d932a8e
USERCHI-F39IHR7\Administrator <yonggangliang@sina.cn>
2017-12-29 11:39:15 +0800

    1px细线

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <router-link tag="div" to="/" class="tab-item" activeClass="on" exact>商品</router-link>
       <router-link tag="div" to="/ratings" class="tab-item" activeClass="on" exact>评论</router-link>
       <router-link tag="div" to="/seller" class="tab-item" activeClass="on" exact>商家</router-link>
@@ -21,6 +21,8 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
+@import "./common/stylus/base.styl"
 html,body{
   margin: 0;
 }
@@ -33,6 +35,7 @@ html,body{
     display: flex;
     height: 40px;
     line-height: 40px;
+    border-1px(rgba(7,17,27,0.1))
     .tab-item{
       flex: 1;
       text-align center;
