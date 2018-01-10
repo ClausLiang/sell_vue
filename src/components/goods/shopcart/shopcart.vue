6d87527827ea24dec5dadef18026f091da3f1cd8
USERCHI-F39IHR7\Administrator <yonggangliang@sina.cn>
2018-01-10 17:10:36 +0800

    购物车完

--- src/components/goods/shopcart/shopcart.vue
+++ src/components/goods/shopcart/shopcart.vue
@@ -3,14 +3,19 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
+          <div class="logo" :class="{'highlight': totalCount > 0}">
             <span class="icon iconfont icon-gouwuche cheche"></span>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">0元</div>
+        <div class="price" :class="{'highlight': totalCount > 0}">￥{{totalPrice}}</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right"></div>
+      <div class="content-right">
+        <div class="pay" :class="payClass">
+          {{payDesc}}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +24,14 @@
   export default {
     name: 'shopcart',
     props: {
+      selectFoods: {
+        type: Array,
+        default () {
+          return [
+            {price: 10, count: 1}
+          ]
+        }
+      },
       deliveryPrice: {
         type: Number,
         default: 0
@@ -26,6 +39,39 @@
       minPrice: {
         type: Number,
         default: 0
+      }
+    },
+    computed: {
+      totalPrice () {
+        let total = 0
+        this.selectFoods.forEach(item => {
+          total += item.price * item.count
+        })
+        return total
+      },
+      totalCount () {
+        let count = 0
+        this.selectFoods.forEach(item => {
+          count += item.count
+        })
+        return count
+      },
+      payDesc () {
+        if (this.totalPrice === 0) {
+          return `${this.minPrice}元起送`
+        } else if (this.totalPrice < this.minPirce) {
+          let diff = this.minPirce - this.totalPrice
+          return `还差${diff}元起送`
+        } else {
+          return '去结算'
+        }
+      },
+      payClass () {
+        if (this.totalPrice > this.minPrice) {
+          return 'enough'
+        } else {
+          return ''
+        }
       }
     }
   }
@@ -43,6 +89,7 @@
       display: flex;
       background: #141d27;
       font-size 0
+      color: rgba(255,255,255,0.4)
       .content-left{
         flex:1;
         .logo-wrapper{
@@ -63,13 +110,35 @@
             border-radius 50%;
             text-align center
             background: #2b343c;
+            &.highlight{
+              background: rgb(0, 160, 220)
+              .cheche{
+                color: #fff;
+              }
+            }
             .cheche{
               line-height: 44px;
               font-size 24px;
               color: #80858a;
             }
           }
+          .num{
+            position: absolute;
+            top:0;
+            right:0;
+            width: 24px;
+            height: 16px;
+            line-height: 16px;
+            border-radius 16px;
+            text-align center
+            font-size 9px;
+            font-weight 700
+            color: #fff;
+            background: rgb(240, 20, 20)
+            box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.4)
+          }
         }
+        /*总价*/
         .price{
           display: inline-block;
           vertical-align top;
@@ -80,20 +149,34 @@
           border-right 1px solid rgba(255,255,255,0.1)
           font-size 16px;
           font-weight 700
-          color: rgba(255,255,255,0.4)
+          &.highlight{
+            color: #fff;
+          }
         }
         .desc{
           display: inline-block;
           vertical-align top
           margin 12px 0 0 12px;
           line-height:24px;
-          color: rgba(255,255,255,0.4)
+
           font-size 10px;
         }
       }
       .content-right{
         flex: 0 0 105px;
         width: 105px;
+        .pay{
+          height 48px;
+          line-height: 48px;
+          text-align center
+          font-size 12px;
+          font-weight 700
+          background: #2b333b;
+          &.enough{
+            background: #00b43c;
+            color: #fff;
+          }
+        }
       }
     }
   }
