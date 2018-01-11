3a5f40862200bf516bdd74d7e5e05d5cb43a5269
USERCHI-F39IHR7\Administrator <yonggangliang@sina.cn>
2018-01-11 16:58:18 +0800

    cartcontrol减号的移动及旋转动效
    通过transation及transform实现

--- src/components/goods/cartcontrol/cartcontrol.vue
+++ src/components/goods/cartcontrol/cartcontrol.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon iconfont icon-jian" v-show="food.count>0" @click="decreaseCart"></div>
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <span class="icon iconfont icon-jian inner"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon iconfont icon-jia" @click="addCart"></div>
   </div>
@@ -35,12 +39,26 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
   .cartcontrol{
     font-size 0
-    .cart-decrease, .cart-add{
+    .cart-decrease{
       display inline-block
       padding: 6px;
-      line-height: 24px;
-      font-size 24px;
-      color: rgb(0, 160, 220)
+      /*减号移动效果*/
+      transition: all 0.4s linear
+      &.move-enter, &.move-leave-active {
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner{
+          transform: rotate(180deg)/*减号旋转效果*/
+        }
+      }
+      .inner{
+        display: inline-block;
+        line-height: 24px;
+        font-size 24px;
+        color: rgb(0, 160, 220)
+        transition: all 0.4s linear/*减号旋转效果*/
+        transform: rotate(0)/*减号旋转效果*/
+      }
     }
     .cart-count{
       display: inline-block
@@ -53,6 +71,11 @@
       color: rgb(147, 153, 159)
     }
     .cart-add{
+      display inline-block
+      padding: 6px;
+      line-height: 24px;
+      font-size 24px;
+      color: rgb(0, 160, 220)
     }
   }
 </style>
